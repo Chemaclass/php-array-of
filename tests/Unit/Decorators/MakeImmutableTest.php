1fee32290eb2975fa40753806b8ea4d26d6bbaad
Katarn <katarn@katarn.es>
2021-03-01 19:48:00 +0100

    Also move the test to decorators

--- tests/Unit/Decorators/MakeImmutableTest.php
+++ tests/Unit/Decorators/MakeImmutableTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace ArrayOfTest\Unit\Traits;
+namespace ArrayOfTest\Unit\Decorators;
 
 use ArrayOf\Decorators\MakeImmutable;
 use ArrayOf\Exceptions\ImmutabilityException;
