8027aa87085fd05f3ec00002d10621ba98ee94a1
Chemaclass <chemaclass@outlook.es>
2021-03-02 08:21:12 +0100

    initial example/index.php

--- example/index.php
+++ example/index.php
@@ -2,3 +2,62 @@
 
 declare(strict_types = 1);
 
+use ArrayOf\AbstractArrayOf;
+use ArrayOf\Decorators\MakeImmutable;
+
+require getcwd() . '/vendor/autoload.php';
+
+final class Article
+{
+    private int $id;
+    private string $name;
+
+    public function __construct(int $id, string $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    public function id(): int
+    {
+        return $this->id;
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function __toString(): string
+    {
+        return "Article {id: $this->id, name: '$this->name'}";
+    }
+}
+
+final class ArrayOfArticles extends AbstractArrayOf
+{
+    public function __construct(array $input = [])
+    {
+        parent::__construct($input);
+        new MakeImmutable($this);
+    }
+
+    protected function typeToEnforce(): string
+    {
+        return Article::class;
+    }
+}
+
+function renderArticles(ArrayOfArticles $articles): void
+{
+    foreach ($articles as $article) {
+        echo $article . PHP_EOL;
+    }
+}
+
+$articles = new ArrayOfArticles([
+    new Article(1, 'article-1'),
+    new Article(2, 'article-2'),
+]);
+
+renderArticles($articles);
